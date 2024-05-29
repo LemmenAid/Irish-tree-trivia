4ea6e1f785ad392446a050194c6afbd7053c315f
Karlan Lemmen <karlanlemmen@gmail.com>
2024-05-29 09:54:21 +0000

    Add function to show questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -225,18 +225,3 @@ function startQuiz() {
     showQuestion();
 }
 
-function showQuestion() {
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
-
-
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.text;
-        button.classList.add("btn");
-        answerButton.appendChild(button);
-    });
-}
-
-startQuiz();
